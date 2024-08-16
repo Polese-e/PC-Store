6a4868dc7b0ebdb3235b44fa64f0529d9169b712
Polese <enzopolesedamasceno@gmail.com>
2024-08-16 13:55:40 -0300

    Update

--- resources/views/partials/card.blade.php
+++ resources/views/partials/card.blade.php
@@ -1,6 +1,6 @@
 <div id="cards-container" class="row mx-auto">
     @foreach ($products as $product)
-        <div class="card col-lg-1-5 p-0">
+        <div class="card col-lg-1-5">
             <div class="promotion-time d-flex">
                 <div class="promotion d-flex align-items-center justify-content-center">
                     <span><strong>{{ $product->discount_percentage }}%</strong></span>
@@ -14,8 +14,25 @@
             <div class="card-image d-flex align-items-center justify-content-center">
                 <img src="/img/products/{{ $product->image }}" alt="{{ $product->title }}">
             </div>
-            <div class="card-body">
-                <h5 class="product-name">{{ $product->name }}</h5>
+            <div class="product-name">
+                <p>{{ $product->name }}</p>
+            </div>
+            <div class="product-price w-100">
+                <div class="old-price">
+                    <p><del>De: R$ 3.171,04</del> por:</p>
+                </div>
+                <div class="new-price d-flex align-items-center">
+                    <p class="d-flex align-items-center">
+                        <span>R$ 1.299,90</span>
+                         à vista
+                    </p>
+                </div>
+                <div class="juros">
+                    <p>12x de R$ 127,44 sem juros</p>
+                </div>
+            </div>
+            <div class="add-to-cart text-center">
+                <span>ADICIONAR AO CARRINHO</span>
             </div>
         </div>
     @endforeach
