c681c7c7b7eed70109a56bfe08d9598e36753a8b
Polese <enzopolesedamasceno@gmail.com>
2024-08-27 21:46:49 -0300

    Update

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -5,18 +5,19 @@
 @include('partials.carousel')
 
 @section('content')
-    <div id="productFeatured" class="container bg-white">
-        <div class="gallery">
-            <div class="main-image">
+    <div id="productFeatured" class="container bg-white d-flex">
+        <div class="gallery d-flex">
+            <div class="thumbnails">
+                <img src="{{ asset('img/products/a32d029808e281f6abb79a08a9d6f012.jpg') }}" alt="Imagem 1"
+                    class="thumbnail active">
+                <img src="{{ asset('img/products/ab64da44b55384c21b71d18169d431be.webp') }}" alt="Imagem 2"
+                    class="thumbnail">
+            </div>
+            <div class="main-image d-flex align-items-center justify-content-center">
                 <img src="{{ asset('img/products/' . $product->image) }}" alt="Imagem 1" class="active">
             </div>
-            <div class="thumbnails">
-                <img src="{{ asset('img/products/' . $product->image) }}" alt="Imagem 1" class="thumbnail active">
-                <img src="{{ asset('img/products/a32d029808e281f6abb79a08a9d6f012.jpg') }}" alt="Imagem 2"
-                    class="thumbnail">
-                <img src="{{ asset('img/products/ab64da44b55384c21b71d18169d431be.webp') }}" alt="Imagem 3"
-                    class="thumbnail">
-            </div>
+        </div>
+        <div class="promotion-area">
         </div>
     </div>
 
